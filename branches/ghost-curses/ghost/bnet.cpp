9d686b6506d8359c7672486c5d6607455eb38b74
smokemnstr <smokemnstr@a7494f72-a4b0-11dd-a887-7ffe1a420f8d>
2010-04-17 12:09:10 +0000

    Ghost-curses branch: Applied fixes from trunk r419.

--- branches/ghost-curses/ghost/bnet.cpp
+++ branches/ghost-curses/ghost/bnet.cpp
@@ -2412,8 +2412,6 @@ void CBNET :: QueueGameRefresh( unsigned char state, string gameName, string hos
 
 	if( m_LoggedIn && map )
 	{
-		BYTEARRAY MapGameType;
-
 		// construct a fixed host counter which will be used to identify players from this realm
 		// the fixed host counter's 4 most significant bits will contain a 4 bit ID (0-15)
 		// the rest of the fixed host counter will contain the 28 least significant bits of the actual host counter
@@ -2421,16 +2419,16 @@ void CBNET :: QueueGameRefresh( unsigned char state, string gameName, string hos
 		// when a player joins a game we can obtain the ID from the received host counter
 		// note: LAN broadcasts use an ID of 0, battle.net refreshes use an ID of 1-10, the rest are unused
 
-		uint32_t FixedHostCounter = ( hostCounter & 0x0FFFFFFF ) | ( GetRealmId( ) << 28 );
-
-		// construct the correct SID_STARTADVEX3 packet
+		uint32_t FixedHostCounter = ( hostCounter & 0x0FFFFFFF ) | ( m_HostCounterID << 28 );
 
 		if( saveGame )
 		{
-			MapGameType.push_back( 0 );
-			MapGameType.push_back( 10 );
-			MapGameType.push_back( 0 );
-			MapGameType.push_back( 0 );
+			uint32_t MapGameType = MAPGAMETYPE_SAVEDGAME;
+
+			// the state should always be private when creating a saved game
+
+			if( state == GAME_PRIVATE )
+				MapGameType |= MAPGAMETYPE_PRIVATEGAME;
 
 			// use an invalid map width/height to indicate reconnectable games
 
@@ -2442,16 +2440,17 @@ void CBNET :: QueueGameRefresh( unsigned char state, string gameName, string hos
 			MapHeight.push_back( 7 );
 
 			if( m_GHost->m_Reconnect )
-				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, MapGameType, map->GetMapGameFlags( ), MapWidth, MapHeight, gameName, hostName, upTime, "Save\\Multiplayer\\" + saveGame->GetFileNameNoPath( ), saveGame->GetMagicNumber( ), map->GetMapSHA1( ), FixedHostCounter ) );
+				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, UTIL_CreateByteArray( MapGameType, false ), map->GetMapGameFlags( ), MapWidth, MapHeight, gameName, hostName, upTime, "Save\\Multiplayer\\" + saveGame->GetFileNameNoPath( ), saveGame->GetMagicNumber( ), map->GetMapSHA1( ), FixedHostCounter ) );
 			else
-				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, MapGameType, map->GetMapGameFlags( ), UTIL_CreateByteArray( (uint16_t)0, false ), UTIL_CreateByteArray( (uint16_t)0, false ), gameName, hostName, upTime, "Save\\Multiplayer\\" + saveGame->GetFileNameNoPath( ), saveGame->GetMagicNumber( ), map->GetMapSHA1( ), FixedHostCounter ) );
+				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, UTIL_CreateByteArray( MapGameType, false ), map->GetMapGameFlags( ), UTIL_CreateByteArray( (uint16_t)0, false ), UTIL_CreateByteArray( (uint16_t)0, false ), gameName, hostName, upTime, "Save\\Multiplayer\\" + saveGame->GetFileNameNoPath( ), saveGame->GetMagicNumber( ), map->GetMapSHA1( ), FixedHostCounter ) );
 		}
 		else
 		{
-			MapGameType.push_back( map->GetMapGameType( ) );
-			MapGameType.push_back( 32 );
-			MapGameType.push_back( 73 );
-			MapGameType.push_back( 0 );
+			uint32_t MapGameType = map->GetMapGameType( );
+			MapGameType |= MAPGAMETYPE_UNKNOWN0;
+
+			if( state == GAME_PRIVATE )
+				MapGameType |= MAPGAMETYPE_PRIVATEGAME;
 
 			// use an invalid map width/height to indicate reconnectable games
 
@@ -2463,9 +2462,9 @@ void CBNET :: QueueGameRefresh( unsigned char state, string gameName, string hos
 			MapHeight.push_back( 7 );
 
 			if( m_GHost->m_Reconnect )
-				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, MapGameType, map->GetMapGameFlags( ), MapWidth, MapHeight, gameName, hostName, upTime, map->GetMapPath( ), map->GetMapCRC( ), map->GetMapSHA1( ), FixedHostCounter ) );
+				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, UTIL_CreateByteArray( MapGameType, false ), map->GetMapGameFlags( ), MapWidth, MapHeight, gameName, hostName, upTime, map->GetMapPath( ), map->GetMapCRC( ), map->GetMapSHA1( ), FixedHostCounter ) );
 			else
-				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, MapGameType, map->GetMapGameFlags( ), map->GetMapWidth( ), map->GetMapHeight( ), gameName, hostName, upTime, map->GetMapPath( ), map->GetMapCRC( ), map->GetMapSHA1( ), FixedHostCounter ) );
+				m_OutPackets.push( m_Protocol->SEND_SID_STARTADVEX3( state, UTIL_CreateByteArray( MapGameType, false ), map->GetMapGameFlags( ), map->GetMapWidth( ), map->GetMapHeight( ), gameName, hostName, upTime, map->GetMapPath( ), map->GetMapCRC( ), map->GetMapSHA1( ), FixedHostCounter ) );
 		}
 	}
 }
