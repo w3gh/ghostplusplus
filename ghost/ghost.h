f5df754260bf89b7d8d755b50594a4221deb52ae
emmeran@gmx.net <emmeran@gmx.net@a7494f72-a4b0-11dd-a887-7ffe1a420f8d>
2010-04-19 12:03:18 +0000

    added ghost.qt branch

--- ghost/ghost.h
+++ ghost/ghost.h
@@ -6,7 +6,7 @@
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+	   http://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
@@ -133,8 +133,9 @@ public:
 	uint32_t m_ReplayBuildNumber;			// config value: replay build number (for saving replays)
 	bool m_TCPNoDelay;						// config value: use Nagle's algorithm or not
 	uint32_t m_MatchMakingMethod;			// config value: the matchmaking method
+	string m_ConfigFile;
 
-	CGHost( CConfig *CFG );
+	CGHost( CConfig *CFG, string configFile );
 	~CGHost( );
 
 	// processing functions
