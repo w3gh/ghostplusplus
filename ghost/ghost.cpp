a5b97a9873a73788f4b48b78dd680ab1832ea300
smokemnstr <smokemnstr@a7494f72-a4b0-11dd-a887-7ffe1a420f8d>
2010-07-25 19:26:04 +0000

    ghost-curses: fixes crash with "ui_enabled = 0"

--- ghost/ghost.cpp
+++ ghost/ghost.cpp
@@ -510,7 +510,7 @@ int main( int argc, char **argv )
 		// block for 50ms on all sockets - if you intend to perform any timed actions more frequently you should change this
 		// that said it's likely we'll loop more often than this due to there being data waiting on one of the sockets but there aren't any guarantees
 
-		if( gGHost->Update( 50000 ) || gUI->update( ) )
+		if( gGHost->Update( 50000 ) || ( gUI && gUI->update( ) ) )
 			break;
 	}
 
