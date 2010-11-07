0192d08f7b7ec3cb38e0334f9c35cc6ea15b4e43
MyFakeAcc.4@googlemail.com <MyFakeAcc.4@googlemail.com@a7494f72-a4b0-11dd-a887-7ffe1a420f8d>
2010-11-07 17:36:29 +0000

    [ghost-genie] Catching up to trunk (r557 & r558)

--- branches/ghost-genie/ghost/gameslot.cpp
+++ branches/ghost-genie/ghost/gameslot.cpp
@@ -25,7 +25,8 @@
 // CGameSlot
 //
 
-CGameSlot :: CGameSlot( BYTEARRAY &n )
+CGameSlot :: CGameSlot( BYTEARRAY &n ) : m_PID( 0 ), m_DownloadStatus( 255 ), m_SlotStatus( SLOTSTATUS_OPEN ), m_Computer( 0 ), m_Team( 0 ), 
+													m_Colour( 1 ), m_Race( SLOTRACE_RANDOM ), m_ComputerType( SLOTCOMP_NORMAL ), m_Handicap( 100 )
 {
 	if( n.size( ) >= 7 )
 	{
@@ -39,25 +40,9 @@ CGameSlot :: CGameSlot( BYTEARRAY &n )
 
 		if( n.size( ) >= 8 )
 			m_ComputerType = n[7];
-		else
-			m_ComputerType = SLOTCOMP_NORMAL;
 
 		if( n.size( ) >= 9 )
 			m_Handicap = n[8];
-		else
-			m_Handicap = 100;
-	}
-	else
-	{
-		m_PID = 0;
-		m_DownloadStatus = 255;
-		m_SlotStatus = SLOTSTATUS_OPEN;
-		m_Computer = 0;
-		m_Team = 0;
-		m_Colour = 1;
-		m_Race = SLOTRACE_RANDOM;
-		m_ComputerType = SLOTCOMP_NORMAL;
-		m_Handicap = 100;
 	}
 }
 
