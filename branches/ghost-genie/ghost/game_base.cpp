46a61ed6bf631b070c017c76a50b86b87458eefe
MyFakeAcc.4@googlemail.com <MyFakeAcc.4@googlemail.com@a7494f72-a4b0-11dd-a887-7ffe1a420f8d>
2011-07-19 08:47:21 +0000

    see: r571

--- branches/ghost-genie/ghost/game_base.cpp
+++ branches/ghost-genie/ghost/game_base.cpp
@@ -1670,34 +1670,32 @@ void CBaseGame :: EventPlayerJoined( CPotentialPlayer *potential, CIncomingJoinP
 	uint32_t HostCounterID = joinPlayer->GetHostCounter( ) >> 28;
 	string JoinedRealm;
 
-	// we use an ID value of 0 to denote joining via LAN
-
-	if( HostCounterID == 0 )
+   
+   for( vector<CBNET *> :: iterator i = m_GHost->m_BNETs.begin( ); i != m_GHost->m_BNETs.end( ); ++i )
+	{
+		if( (*i)->GetHostCounterID( ) == HostCounterID )
+			JoinedRealm = (*i)->GetServer( );
+	}
+   
+	if( JoinedRealm.empty( ) )
 	{
 		// the player is pretending to join via LAN, which they might or might not be (i.e. it could be spoofed)
 		// however, we've been broadcasting a random entry key to the LAN
 		// if the player is really on the LAN they'll know the entry key, otherwise they won't
 		// or they're very lucky since it's a 32 bit number
-
+      
 		if( joinPlayer->GetEntryKey( ) != m_EntryKey )
 		{
 			// oops!
-
+         
 			CONSOLE_Print( "[GAME: " + m_GameName + "] player [" + joinPlayer->GetName( ) + "|" + potential->GetExternalIPString( ) + "] is trying to join the game over LAN but used an incorrect entry key" );
 			potential->Send( m_Protocol->SEND_W3GS_REJECTJOIN( REJECTJOIN_WRONGPASSWORD ) );
 			potential->SetDeleteMe( true );
 			return;
 		}
 	}
-	else
-	{
-                for( vector<CBNET *> :: iterator i = m_GHost->m_BNETs.begin( ); i != m_GHost->m_BNETs.end( ); ++i )
-		{
-			if( (*i)->GetHostCounterID( ) == HostCounterID )
-				JoinedRealm = (*i)->GetServer( );
-		}
-	}
-
+   
+   
 	// check if the new player's name is banned but only if bot_banmethod is not 0
 	// this is because if bot_banmethod is 0 and we announce the ban here it's possible for the player to be rejected later because the game is full
 	// this would allow the player to spam the chat by attempting to join the game multiple times in a row
