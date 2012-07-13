d748c371ad2803ae4e92452068b255bc308f155a
AbsoluteMSTR@gmail.com <AbsoluteMSTR@gmail.com@a7494f72-a4b0-11dd-a887-7ffe1a420f8d>
2012-07-13 06:24:57 +0000

    added bot config value bot_mapgametype updated changelog.txt to reflect changes made since 17.1 project file updated to vc2010 compiled ghost.exe potential candidate for 17.2

--- ghost/ghost.h
+++ ghost/ghost.h
@@ -1,20 +1,20 @@
 /*
 
-   Copyright [2008] [Trevor Hogan]
+Copyright [2008] [Trevor Hogan]
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-   CODE PORTED FROM THE ORIGINAL GHOST PROJECT: http://ghost.pwner.org/
+CODE PORTED FROM THE ORIGINAL GHOST PROJECT: http://ghost.pwner.org/
 
 */
 
@@ -133,6 +133,7 @@ public:
 	uint32_t m_ReplayBuildNumber;			// config value: replay build number (for saving replays)
 	bool m_TCPNoDelay;						// config value: use Nagle's algorithm or not
 	uint32_t m_MatchMakingMethod;			// config value: the matchmaking method
+	uint32_t m_MapGameType;                 // config value: the MapGameType overwrite (aka: refresh hack)
 
 	CGHost( CConfig *CFG );
 	~CGHost( );
